6bbb40c41aefbc17812001ab82e70f9604ce0c8c
Ivan Andronowicz <ivanandronowicz@Ivans-MacBook.local>
2016-07-26 19:59:38 -0300

    Fix latest migration was rails 5 error

--- db/migrate/20160724150038_default_date_trabajos.rb
+++ db/migrate/20160724150038_default_date_trabajos.rb
@@ -1,4 +1,4 @@
-class DefaultDateTrabajos < ActiveRecord::Migration[5.0]
+class DefaultDateTrabajos < ActiveRecord::Migration
   def change
   	execute "ALTER TABLE trabajos CHANGE fecha_de_inicio fecha_de_inicio TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;"
   end
